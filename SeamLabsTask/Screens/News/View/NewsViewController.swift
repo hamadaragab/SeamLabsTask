25d85d1f46d99ac4f2be810daaddc39735bb0e5a
hamadaRagab <hamaadragab550@gmail.com>
2023-09-04 11:08:10 +0300

    app Configurations

---
+++ SeamLabsTask/Screens/News/View/NewsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewsViewController.swift
+//  SeamLabsTask
+//
+//  Created by Hamada Ragab on 04/09/2023.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

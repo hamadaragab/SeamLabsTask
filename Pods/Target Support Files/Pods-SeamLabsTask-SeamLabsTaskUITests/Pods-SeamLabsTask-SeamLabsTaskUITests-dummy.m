238a078185ba3cad8f4c13e3b1c24a96812c613b
hamadaRagab <hamaadragab550@gmail.com>
2023-09-18 15:16:25 +0300

    pod files not pushed to gitHub

---
+++ Pods/Target Support Files/Pods-SeamLabsTask-SeamLabsTaskUITests/Pods-SeamLabsTask-SeamLabsTaskUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeamLabsTask_SeamLabsTaskUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SeamLabsTask_SeamLabsTaskUITests
+@end
---
+++ Pods/Target Support Files/Pods-SeamLabsTask/Pods-SeamLabsTask-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SeamLabsTask : NSObject
+@end
+@implementation PodsDummy_Pods_SeamLabsTask
+@end
